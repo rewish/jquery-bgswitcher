d3e17f0f18a36602997b48d9854c3d618c1fa0c3
rewish <rewish.org@gmail.com>
2011-07-22 12:19:08 +0900

    Version 0.3.3-beta released

--- jquery.bgSwitcher.js
+++ jquery.bgSwitcher.js
@@ -1,7 +1,7 @@
 /*!
  * jQuery.bgSwitcher
  *
- * @version    0.3.2-beta
+ * @version    0.3.3-beta
  * @author     Hiroshi Hoaki <rewish.org@gmail.com>
  * @copyright  2010-2011 Hiroshi Hoaki
  * @license    http://rewish.org/license/mit The MIT License
